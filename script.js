9d0c709f079cdb277b37bf6a8187fff49d8e7ef9
alzlatoverov <alzlatoverov@gmail.com>
2024-05-10 16:27:08 +0400

    Add comic book images

---
+++ script.js
@@ -0,0 +1,14 @@
+window.addEventListener('load', function () {
+    for (let imgIdx = 1; imgIdx <= 33; imgIdx++) {
+        var imgDiv = document.createElement('div')
+        imgDiv.className = "pure-u-1-1 pure-u-lg-1-2"
+
+        var img = document.createElement('img')
+        img.className = "pure-img"
+        img.src = `/img/comics/${imgIdx}.jpg`
+        img.alt = `Страница ${imgIdx}`
+        imgDiv.appendChild(img)
+
+        document.getElementById('main-img-grid').appendChild(imgDiv)
+    }
+})
